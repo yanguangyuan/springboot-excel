6fc74d08c413c5f48d44776d557edee39c4d2ea6
yanguangyuan <1847344456@qq.com>
2018-12-25 15:24:24 +0800

    类注释

--- src/main/java/com/ygy/springbootexcel/SpringbootExcelApplication.java
+++ src/main/java/com/ygy/springbootexcel/SpringbootExcelApplication.java
@@ -2,7 +2,13 @@ package com.ygy.springbootexcel;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+/**
+ * project_name: springboot-excel
+ * describe: 启动类
+ *
+ * @author : yanguangyuan
+ * creat_date: 2018-12-25 15:11
+ **/
 @SpringBootApplication
 public class SpringbootExcelApplication {
 
